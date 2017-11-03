5a5fd55e99427156ba69e65d1e4d3f1bc901eb6f
Seunghyun.hong <hong.seung.hyun.23@gmail.com>
2017-11-03 11:47:54 +0900

    DatePickerDialog 로 날짜 선택후, 날짜 보여주고 엔터누르면 출력

--- app/src/main/java/com/hseunghyun/runacaculater/MainActivity.java
+++ app/src/main/java/com/hseunghyun/runacaculater/MainActivity.java
@@ -1,11 +1,11 @@
 package com.hseunghyun.runacaculater;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.hseunghyun.runacaculater.model.Runa;
@@ -20,12 +20,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    /**
-     * EditText 를 이용한 날짜 입력.
-     */
-    private EditText mInputSolaYearEditText;
-    private EditText mInputSolaMonthEditText;
-    private EditText mInputSolaDayEditText;
+//    /**
+//     * EditText 를 이용한 날짜 입력.
+//     */
+//    private EditText mInputSolaYearEditText;
+//    private EditText mInputSolaMonthEditText;
+//    private EditText mInputSolaDayEditText;
 
     private RunaApi mRunaApiservice;
 
@@ -33,26 +33,35 @@ public class MainActivity extends AppCompatActivity {
     private TextView outputRunResult;
     private TextView outputRunaResult;
 
+//    /**
+//     * datepicker 를 이용한 날짜 선택
+//     */
+//    private DatePicker mDatePicker;
+
     /**
-     * datepicker 를 이용한 날짜 선택
+     * datepicker 다이얼로그
      */
-    private DatePicker mDatePicker;
+    private TextView mDatepickerTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        /**
+//         * EditText 를 이용한 날짜 입력.
+//         */
+//        mInputSolaYearEditText = findViewById(R.id.input_sola_year);
+//        mInputSolaMonthEditText = findViewById(R.id.input_sola_month);
+//        mInputSolaDayEditText = findViewById(R.id.input_sola_day);
+//        /**
+//         * datepicker 를 이용한 날짜 선택
+//         */
+//        mDatePicker = findViewById(R.id.date_picker);
         /**
-         * EditText 를 이용한 날짜 입력.
+         * datepicker 다이얼로그
          */
-        mInputSolaYearEditText = findViewById(R.id.input_sola_year);
-        mInputSolaMonthEditText = findViewById(R.id.input_sola_month);
-        mInputSolaDayEditText = findViewById(R.id.input_sola_day);
-        /**
-         * datepicker 를 이용한 날짜 선택
-         */
-        mDatePicker = findViewById(R.id.date_picker);
+        mDatepickerTextview = findViewById(R.id.datepicker_textview);
 
 
         outputRunResult = findViewById(R.id.output_run_result);
@@ -65,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
 
         mRunaApiservice = retrofit.create(RunaApi.class);
 
+
     }
+
 
     public void onClickedEnterButton(View view) {
         converter();
@@ -76,44 +87,59 @@ public class MainActivity extends AppCompatActivity {
     // 레트로핏 부르고 . api에 뭘로 쿼리 할꺼였는지 그 이름 적어주고. 인큐(new call...)
     public void converter() {
 
-        /**
-         * EditText 를 이용한 날짜 입력.
-         */
-        // 년 월 일 따로 담는다.
-        String year = mInputSolaYearEditText.getText().toString();
+//        /**
+//         * EditText 를 이용한 날짜 입력.
+//         */
+//        // 년 월 일 따로 담는다.
+//        String year = mInputSolaYearEditText.getText().toString();
+//        String month = null;
+//        if (mInputSolaMonthEditText.getText().toString().length() < 2) {
+//            month = "0" + mInputSolaMonthEditText.getText().toString();
+//        } else {
+//            month = mInputSolaMonthEditText.getText().toString();
+//        }
+////        Toast.makeText(this, "달"+month, Toast.LENGTH_SHORT).show();
+//        String day = null;
+//        if (mInputSolaDayEditText.getText().toString().length() < 2) {
+//            day = "0" + mInputSolaDayEditText.getText().toString();
+//        } else {
+//            day = mInputSolaDayEditText.getText().toString();
+//        }
+//
+//        /**
+//         * datepicker 를 이용한 날짜 선택
+//         */
+//        String year = String.valueOf(mDatePicker.getYear());
+//
+//        // 달은 0~11이기 때문에 +1 해주고 월 일 0도 붙여 줘야 한다.
+//        String month = null;
+//        if (mDatePicker.getMonth() + 1 < 10) {
+//            month = "0" + String.valueOf(mDatePicker.getMonth() + 1);
+//        } else {
+//            month = String.valueOf(mDatePicker.getMonth());
+//        }
+//
+//        String day = null;
+//        if (mDatePicker.getDayOfMonth() < 10) {
+//            day = "0" + String.valueOf(mDatePicker.getDayOfMonth());
+//        } else {
+//            day = String.valueOf(mDatePicker.getDayOfMonth());
+//        }
+
+        String year = String.valueOf(mYear);
         String month = null;
-        if (mInputSolaMonthEditText.getText().toString().length() < 2) {
-            month = "0" + mInputSolaMonthEditText.getText().toString();
+        if (mMonth + 1 < 10) {
+            month = "0" + String.valueOf(mMonth + 1);
         } else {
-            month = mInputSolaMonthEditText.getText().toString();
+            month = String.valueOf(mMonth + 1);
         }
-//        Toast.makeText(this, "달"+month, Toast.LENGTH_SHORT).show();
         String day = null;
-        if (mInputSolaDayEditText.getText().toString().length() < 2) {
-            day = "0" + mInputSolaDayEditText.getText().toString();
+        if (mDay < 10) {
+            day = "0" + String.valueOf(mDay);
         } else {
-            day = mInputSolaDayEditText.getText().toString();
+            day = String.valueOf(mDay);
         }
 
-        /**
-         * datepicker 를 이용한 날짜 선택
-         */
-        String year = String.valueOf(mDatePicker.getYear());
-
-        // 달은 0~11이기 때문에 +1 해주고 월 일 0도 붙여 줘야 한다.
-        String month = null;
-        if (mDatePicker.getMonth() + 1 < 10) {
-            month = "0" + String.valueOf(mDatePicker.getMonth() + 1);
-        } else {
-            month = String.valueOf(mDatePicker.getMonth());
-        }
-
-        String day = null;
-        if (mDatePicker.getDayOfMonth() + 1 < 10) {
-            day = "0" + String.valueOf(mDatePicker.getDayOfMonth() + 1);
-        } else {
-            day = String.valueOf(mDatePicker.getDayOfMonth());
-        }
 
         mRunaApiservice.caculaterDay(year, month, day).enqueue(new Callback<Runa>() {
             @Override // 성공
@@ -135,20 +161,51 @@ public class MainActivity extends AppCompatActivity {
 
             @Override // 실패
             public void onFailure(Call<Runa> call, Throwable t) {
-                if (t.getLocalizedMessage().equals("Unable to resolve host \"apis.data.go.kr\": No address associated with hostname")) {
-                    outputRunResult.setText("인터넷접속이 되어 있는지 확인해주세요");
-
-                } else if (t.getLocalizedMessage().equals("java.lang.IllegalStateException: Expected BEGIN_OBJECT but was STRING at line 1 column 90 path $.response.body.items")) {
-                    outputRunResult.setText("년도 월 일을 다시 한번 확인해 주세요");
-                } else {
-                    outputRunResult.setText(t.getLocalizedMessage());
-                }
+                // 널포인트될수 있어서 지움.
+//                if (t.getLocalizedMessage().equals("Unable to resolve host \"apis.data.go.kr\": No address associated with hostname")) {
+//                    outputRunResult.setText("인터넷접속이 되어 있는지 확인해주세요");
+//
+//                } else if (t.getLocalizedMessage().equals("java.lang.IllegalStateException: Expected BEGIN_OBJECT but was STRING at line 1 column 90 path $.response.body.items")) {
+//                    outputRunResult.setText("년도 월 일을 다시 한번 확인해 주세요");
+//                } else {
+//                    outputRunResult.setText(t.getLocalizedMessage());
+//                }
                 Log.d(TAG, "onFailure: " + t.getLocalizedMessage());
 //                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
 
+    }
+
+    /**
+     * datepicker 다이얼로그
+     */
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
+    public void onClickedDateSelectButton(View view) {
+        new DatePickerDialog(MainActivity.this, mDateSetListener, mYear, mMonth, mDay).show();
+    }
+
+    DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            //사용자가 입력한 값을 가져온뒤
+            mYear = year;
+            mMonth = month;
+            mDay = dayOfMonth;
+
+            //텍스트뷰의 값을 업데이트함
+            UpdateNow();
+        }
+    };
+
+    //텍스트뷰의 값을 업데이트 하는 메소드
+
+    void UpdateNow() {
+        mDatepickerTextview.setText(String.format("%d/%d/%d", mYear, mMonth + 1, mDay));
     }
 
 }
